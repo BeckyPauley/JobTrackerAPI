097ccc146a78b78345b66bff50de59b7d9ce322a
Becky P <64131207+BeckyPauley@users.noreply.github.com>
2020-08-12 14:35:55 +0100

    Adds improvements to readability of basic app

--- code/app.py
+++ code/app.py
@@ -9,61 +9,74 @@ jobs = []
 
 class Job(Resource):
     parser = reqparse.RequestParser()
-    parser.add_argument('company', 
-        type=str, 
-        required=True, 
-        help="This field cannot be left blank" 
-        ) 
+    parser.add_argument(
+        'company',
+        type=str,
+        required=True,
+        help="This field cannot be left blank"
+        )
     parser.add_argument('salary')
     parser.add_argument('status')
 
     def post(self, title):
 
-        if next(filter(lambda x: x['title'] == title, jobs), None) is not None:
-            return{'message': "A job with name '{}' already exists".format(title)}, 400 
-        data = Job.parser.parse_args() 
+        if self.check_for_job(title) is not None:
+            return{'message': "A job with name '{}' already exists".format(title)}, 400
+
+        data = Job.parser.parse_args()
 
         job = {
-                'title': title, 
-                'company': data['company'], 
-                'salary': data['salary'], 
-                'status': data['status']
-                }
+            'title': title, 
+            'company': data['company'],
+            'salary': data['salary'],
+            'status': data['status']
+            }
         jobs.append(job)
 
         return job, 201
 
+    def check_for_job(self, title):
+        for job in jobs:
+            if job['title'] == title:
+                return job
+        return None
+
     def get(self, title):
-        job  = next(filter(lambda x: x['title'] == title, jobs), None) # Returns a filter object, returns None if can;t find object
-        return {'job': job}, 200 if job else 404
+        job = self.check_for_job(title)
+        if job:
+            return {'job': job}, 200
+        else:
+            return{"message": "Job does not exist"}, 404
 
     def put(self, title):
 
         data = Job.parser.parse_args()
 
-        job = next(filter(lambda x: x['title'] == title, jobs), None)
+        job = self.check_for_job(title)
         if job is None:
             job = {
-                'title': title, 
-                'company': data['company'], 
-                'salary': data['salary'], 
+                'title': title,
+                'company': data['company'],
+                'salary': data['salary'],
                 'status': data['status']
                 }
-            jobs.append(job)
+            jobs.append(job), 201
         else:
-            job.update(data)
+            job.update(data), 200
         return job
 
     def delete(self, title):
+        if self.check_for_job(title) is None:
+            return{'message': "A job with name '{}' does not exist".format(title)}, 400
         global jobs
-        jobs = list(filter(lambda x: x['title'] != title, jobs))
-        return {'message': 'Item deleted'}
+        jobs = list([job for job in jobs if job['title'] != title])
+        return {'message': 'Item deleted'}, 200
 
 
 class JobsList(Resource):
 
     def get(self):
-        return {"jobs": jobs}
+        return {"jobs": jobs}, 200
 
 api.add_resource(Job, '/job/<string:title>')
 api.add_resource(JobsList, '/jobs')
